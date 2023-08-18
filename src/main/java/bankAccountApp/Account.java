777676deb6d314bddf3b01362e54209d7e137709
Artuqu <percikartur@gmail.com>
2023-08-18 12:25:44 +0200

    Bank Account App init commit

---
+++ src/main/java/bankAccountApp/Account.java
@@ -0,0 +1,25 @@
+package bankAccountApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account implements IBaseRate{
+
+    //    holding data
+    List<Account> accounts = new ArrayList<>();
+    private String name;
+    private String digitAccountNumber;
+    private int socialSecurityNumber;
+    private String accountType;
+    private double initialDeposit;
+
+//    deposit
+
+//    withdraw
+
+//    transfer
+
+//    showInfo
+
+//    generate account number
+}
