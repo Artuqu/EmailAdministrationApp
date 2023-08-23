2bec832f065b81c18ffc552ed8b0811ee54bafb7
Artuqu <percikartur@gmail.com>
2023-08-23 07:40:02 +0200

    Save safety deposit box

--- src/main/java/bankAccountApp/Checking.java
+++ src/main/java/bankAccountApp/Checking.java
@@ -17,7 +17,8 @@ public class Checking extends Account {
 //    List any methods specific to a checking account
     public void showInfo(){
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Checking");
+        System.out.println(" Your Checking Account Features:" +
+                "\n ");
         System.out.println();
     }
 
--- src/main/java/bankAccountApp/Savings.java
+++ src/main/java/bankAccountApp/Savings.java
@@ -1,23 +1,35 @@
 package bankAccountApp;
 
+import java.util.Random;
+
 public class Savings extends Account {
 
 
     //    List properties specific to a savings account
     private int safetyDepositBoxID;
     private int safetyDepositBoxKey;
+    Random random = new Random();
 
 //    Constructor to initialize savings account properties
 
     public Savings(String name, String socialSecurityNumber, double initialDeposit) {
         super(name, socialSecurityNumber, initialDeposit);
         digitAccountNumber = "2" + digitAccountNumber;
+        setSafetyDepositBox();
     }
 
 
-//    List any methods specific to a savings account
-    public void showInfo(){
+    //    List any methods specific to a savings account
+    public void showInfo() {
         super.showInfo();
-        System.out.println("ACCOUNT TYPE: Savings");
+        System.out.println(" Your Savings Account Features:" +
+                "\n Safety Deposit Box ID: " + safetyDepositBoxID
+                + "\n Safety Deposit Box Key: " + safetyDepositBoxKey + "\n");
+    }
+
+
+    private void setSafetyDepositBox() {
+        safetyDepositBoxID = random.nextInt(100, 999);
+        safetyDepositBoxKey = random.nextInt(1000, 9999);
     }
 }
