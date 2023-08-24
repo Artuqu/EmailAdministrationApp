685f14e73fe03481eeb11a21294906f18c4e84ca
Artuqu <percikartur@gmail.com>
2023-08-24 10:45:33 +0200

    Create objects based on CSV File

--- src/main/java/bankAccountApp/BankAccountApp.java
+++ src/main/java/bankAccountApp/BankAccountApp.java
@@ -12,8 +12,18 @@ public class BankAccountApp {
 //        Read a CSV File then create new accounts based on that data
         List<String[]> newAccountHolders = CSV.read(file);
         for (String[] s : newAccountHolders) {
-            for (int i = 0; i < s.length; i++) {
-                System.out.println(s[i]);
+            String name = s[0];
+            String socialSecurityNumber = s[1];
+            String accountType = s[2];
+            double initialDeposit = Double.parseDouble(s[3]);
+            if (accountType.equals("Savings")) {
+                Savings savings = new Savings(name, socialSecurityNumber, initialDeposit);
+                savings.showInfo();
+            } else if (accountType.equals("Checking")) {
+                Checking checking = new Checking(name, socialSecurityNumber, initialDeposit);
+                checking.showInfo();
+            } else {
+                System.out.println("Error");
             }
         }
 
